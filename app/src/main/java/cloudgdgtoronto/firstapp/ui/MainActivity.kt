ebd4139164caa8f1a874c3a80cfb9ec108053a8e
ANNIN_ST_PC <anast3222@gmail.com>
2022-02-11 02:18:34 -0500

    update code : Second Activity added

--- app/src/main/java/cloudgdgtoronto/firstapp/ui/MainActivity.kt
+++ app/src/main/java/cloudgdgtoronto/firstapp/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package cloudgdgtoronto.firstapp
+package cloudgdgtoronto.firstapp.ui
 
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import cloudgdgtoronto.firstapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         //reference the button from the layout
         val button_start = findViewById(R.id.start_button) as Button
 
+        val button_activity = findViewById(R.id.activity_button) as Button
+
         //make the button clickable
         button_start.setOnClickListener(){
 
@@ -22,8 +25,19 @@ class MainActivity : AppCompatActivity() {
             intent.setData(Uri.parse("https://kotlinlang.org"))
             startActivity(intent)
 
+        }
+
+        //make the button clickable
+        button_activity.setOnClickListener(){
+
+            //Requesting an action
+            intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
 
         }
+
+
+
 
     }
 }
---
+++ app/src/main/java/cloudgdgtoronto/firstapp/ui/SecondActivity.kt
@@ -0,0 +1,14 @@
+package cloudgdgtoronto.firstapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cloudgdgtoronto.firstapp.R
+
+class SecondActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+    }
+}
