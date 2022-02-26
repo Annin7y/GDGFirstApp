2d269dd9038bf3066112daf6bdcc7018df69882c
ANNIN_ST_PC <anast3222@gmail.com>
2022-02-26 01:44:56 -0500

    update code : Second Activity ImageView added

--- app/src/main/java/cloudgdgtoronto/firstapp/ui/SecondActivity.kt
+++ app/src/main/java/cloudgdgtoronto/firstapp/ui/SecondActivity.kt
@@ -1,6 +1,10 @@
 package cloudgdgtoronto.firstapp.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import cloudgdgtoronto.firstapp.R
 
@@ -13,6 +17,18 @@ class SecondActivity : AppCompatActivity()
 
         val name = "Lisa"
         var age = 20
+
+        val image_view = findViewById(R.id.imageView) as ImageView
+
+        //make the button clickable
+        image_view.setOnClickListener(){
+
+            //Requesting an action
+            intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("https://kotlinlang.org"))
+            startActivity(intent)
+
+        }
 
     }
 }
